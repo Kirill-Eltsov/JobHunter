7d5f05458ab77c1051382f2ba5f14470969737e9
sasha-production <sasha.ignatov93@gmail.com>
2025-04-26 01:23:38 +0500

    Reapply "update main.py"
    This reverts commit f49f9d70e7b5fd58514f35213791d20a90543022.

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
-from telegram import Update, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackQueryHandler
-from handlers.start_handler import start, button_handler, city_selection_handler, \
-    handle_position_input, salary_selection_handler, handle_position_selection
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, ConversationHandler
+from handlers.start_handler import (
+    start, button_handler, city_selection_handler, handle_position_selection,
+    handle_position_input, salary_selection_handler, show_city_selection,
+    CITY, POSITION, SALARY
+)
 from utils.logger import log_warning
 from config import TOKEN
 
 
 def main():
     """Запускает бота."""
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token("7796049245:AAH6thAwhzEr-J1haDb8Gk6VicWuESjUOek").build()
+    
+    # Обработчик команды /start
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(button_handler, pattern='^choose_vacancies$'))
-    application.add_handler(CallbackQueryHandler(city_selection_handler, pattern='^city_'))
-    application.add_handler(CallbackQueryHandler(handle_position_selection, pattern='^position_'))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_position_input))
-    application.add_handler(CallbackQueryHandler(salary_selection_handler, pattern='^salary_'))
+    
+    # Обработчик для поиска вакансий
+    job_search_conv = ConversationHandler(
+        entry_points=[MessageHandler(filters.Regex('^Поиск вакансий$'), button_handler)],
+        states={
+            CITY: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, city_selection_handler),
+            ],
+            POSITION: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, handle_position_selection),
+            ],
+            SALARY: [
+                MessageHandler(filters.TEXT & ~filters.COMMAND, salary_selection_handler),
+            ],
+        },
+        fallbacks=[CommandHandler("start", start)],
+        name="job_search_conversation",
+        persistent=False
+    )
+    application.add_handler(job_search_conv)
+    
+    # Обработчик для других кнопок главного меню
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, button_handler))
 
     application.run_polling()
 
