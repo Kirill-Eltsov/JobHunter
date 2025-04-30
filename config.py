d64dc3c39a63cf23e0d082823fc2d9fc072d39c9
sasha-production <sasha.ignatov93@gmail.com>
2025-05-01 00:36:19 +0500

    func to get city name by coordinates

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-# import os
-# from dotenv import load_dotenv, dotenv_values
 
-# load_dotenv()
-
-# TOKEN = os.getenv('TOKEN')
 
 # Параметры для поиска вакансий
 SEARCH_PARAMS = {
@@ -14,3 +9,5 @@ SEARCH_PARAMS = {
 
 # URL API для получения вакансий с hh.ru
 HH_API_URL = "https://api.hh.ru/vacancies"
+
+OPEN_STREET_MAP_URL = "https://nominatim.openstreetmap.org/reverse"
---
+++ services/osm_service.py
@@ -0,0 +1,26 @@
+from config import OPEN_STREET_MAP_URL
+
+async def get_city_by_location(lat, lon):
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "format": "json"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(OPEN_STREET_MAP_URL, params=params) as response:
+                response.raise_for_status()
+                data = await response.json()
+                city = data["address"]["city"]
+                return city
+                print(city)
+        except:
+            return "Can't get city"
+
+def main():
+    get_city_by_location('56.765158', '60.544131')
+
+if __name__ == "__main__":
+    main()
+    # get_city_by_location('56.765158', '60.544131')
