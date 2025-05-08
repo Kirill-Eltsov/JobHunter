f8f8259a1097066d8c0abc866c629b4e9c49b043
Username-is-not-avialable <antonbakir280@mail.ru>
2025-05-09 01:59:31 +0500

    add test_db

---
+++ utils/test_db.py
@@ -0,0 +1,15 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.getenv('POSTGRES_HOST'),
+    port=os.getenv('POSTGRES_PORT'),
+    dbname=os.getenv('POSTGRES_DB'),
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD')
+)
+print("Connection successful!")
+conn.close()
